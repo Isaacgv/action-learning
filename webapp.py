d1f442d67466046e445d7251ffdf86d69bbdd134
ibrahim sobh <isobh@outlook.com>
2022-07-28 00:59:11 +0200

    :necktie: adding a new web interface for retraining the model

--- webapp.py
+++ webapp.py
@@ -1,17 +1,8 @@
-
-from ast import IsNot
-import os
-import cv2
-from git import safe_decode
-import numpy as np
-#import av
-import mediapipe as mp
 import streamlit as st
-import mediapipe as mp
 import subprocess
+import os
 from os.path import exists
 import uuid 
-import shutil
 
 import sys
 sys.path.append("..")
@@ -38,25 +29,27 @@ def sign_recognition_video():
     st.markdown(new_title, unsafe_allow_html=True)
     st.markdown("""<p style="font-size: 25px; font-weight:bolder;">
     This Sign Language Detection Model takes in a video as an input and then outputs that video """ +
-    """along side the sign word equivalant in natutral Langauge</br></br> &#129330; &#10133; &#129302; &#10145; Thank you !</p></br>""", unsafe_allow_html=True)
+    """along side the sign word equivalant in natutral Langauge</br></br> &#129330; &#10133; &#129302; &#10145;"""+
+    """ Thank you !</p></br>""", unsafe_allow_html=True)
   
     st.subheader("Option 1 - Upload a video")
     file = st.file_uploader('', type = ['mp4'])
   
     if file is not None:
-        file_details = "File: Name: "+ str(file.name)+", Type: " +str(file.type)
+        file_path= os.path.join("videos/keepers",file.name)
         st.video(file)
         save =st.button("Save Video")  
         if save: 
-            with open(os.path.join("videos/keepers",file.name),"wb") as f: 
+            with open(file_path,"wb") as f: 
                 f.write(file.getbuffer())         
                 st.success("Saved File")
                 process =subprocess.run(["python", "mediapipe/utils/test.py",os.path.join("videos/keepers",file.name)])
                 
-            
+
     st.markdown("<hr style= size='6', color=black> ", unsafe_allow_html=True)
     st.subheader("Option 2 - Redcord using Webcam")
-    st.markdown("<p style='font-size: 20px' ><b>Instructions</b><ul><li>Press ( S ) to Start</li><li>Wait the timer for 3 Seconds</li><li>Press ( Q ) to Quit</p> ", unsafe_allow_html=True)
+    st.markdown("<p style='font-size: 20px' ><b>Instructions</b><ul><li>Press ( S ) to Start</li><li>Wait the "+
+                "timer for 3 Seconds</li><li>Press ( Q ) to Quit</p> ", unsafe_allow_html=True)
     
     
     run =st.button("Launch Webcam")
@@ -82,53 +75,83 @@ def sign_recognition_video():
                 return file_code
             elif delete:
                  pass
-        
-# def sign_recognition_image(): 
-#     new_title = '<p style="font-size: 42px; font-weight:bolder;">Sign Language Recognition for &#127878;<br/></p>'
-#     st.markdown(new_title, unsafe_allow_html=True)
-#     st.markdown("""<p style="font-size: 25px; font-weight:bolder;">
-#     This Sign Language Detection Model takes in an image as an input and then outputs that image with bounding boxes """ +
-#     """describing the ASL equivalant word in natutral Langauge</br></br> &#128075; &#10133; &#129302; &#10145; Hello!</p></br>""", unsafe_allow_html=True)
-  
-#     file = st.file_uploader('', type = ['jpg','png','jpeg'])
-#     if file is not None:
-#         st.image(file)
-#         save =st.button("Save Image")  
-#         if save: 
-#             with open(os.path.join("images",file.name),"wb") as f: 
-#                 f.write(file.getbuffer())         
-#                 st.success("Saved File")
 
+def sign_recognition_Video_retraining():
+    new_title = '<p style="font-size: 42px; font-weight:bolder;">Teach me a Sign &#x1F468;<br/></p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+    st.markdown("""<p style="font-size: 25px; font-weight:bolder;">
+    This Sign Language Detection Model can learn a new Sign Langauge gesture only from taking a video as an input from the user """ +
+    """</br></br> &#127909; &#10133; &#129302; &#129305;"""+
+    """ Thank you !</p></br>""", unsafe_allow_html=True)
+  
+    st.subheader("Redcord using Webcam")
+    st.markdown("<p style='font-size: 20px' ><b>Instructions</b><ul><li>Press ( S ) to Start</li><li>Wait the "+
+                "timer for 3 Seconds</li><li>Press ( Q ) to Quit</p> ", unsafe_allow_html=True)
+    
+    
+    run =st.button("Launch Webcam")
+    file_code =str(uuid.uuid4())[:8]
+    path ="videos/keepers/"+file_code+".mp4"
+    
+    showthem =False
+    if run:
+        process_training =subprocess.run(["python", "camera.py",path])
+       
+        print(path)
+        if process_training.returncode ==0:
+            st.write("Nothing Was Redorded!")
+        
+        elif exists(path):
+            st.write("Video Was Redorded!")
+            st.video(path)
+            title = st.text_input('', 'Sign Title')
+            st.write('Name your Sign Language', title)
+            process_training =subprocess.run(["python", "frames",'videos/keepers/'+file_code+'.mp4',''])
+            keep =st.checkbox("Keep Video")
+            delete=st.checkbox("Delete Video")
+            if keep:
+                st.write("Video Was Submited Sucessefully!!")
+                return file_code
+            elif delete:
+                 pass
+    
 def main():
-    new_title = '<p style="font-size: 42px; font-weight:bolder;">SignMe &#128406;<br/></p><p style="font-size: 38px;">Welcome to our App!</p>'
+    new_title = '<p style="font-size: 42px; font-weight:bolder;">SignMe &#128406;<br/></p><p style="font-size: 32px;">Welcome to our App!</p>'
     read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
 
-    read_me = st.markdown("""<p style="font-size: 26px;text-align: left;">
+    read_me = st.markdown("""<p style="font-size: 24px;text-align: left;">
     This research project is built to demonstrate the importance of <b>Few Shot Learning in Sign Language Recognition</b>.
-    It was built using Streamlit and Mediapipe and many more computer vision Libraries.
-    in both videos(pre-recorded)
-    and images.""",unsafe_allow_html=True)
-    
-    read_repo = st.markdown("""
-     The Github repository can be found 
+    It was built using Streamlit and Mediapipe and many more computer vision Libraries.""",unsafe_allow_html=True)
+    img_0=st.image("images/signme.png", width=660)
+    line_0=st.markdown("<hr style= size='6', color=black> ", unsafe_allow_html=True)
+    img_1=st.image("images/github.png", width=50)
+    read_repo = st.markdown("""Our Github repository can be found 
     [here](https://github.com/Isaacgv/action-learning/tree/main)""")
-    st.sidebar.title("Select Activity")
-    choice  = st.sidebar.selectbox("MODE",("About","Sign Language Recognition(Video)")) # "Sign Language Recognition(Image)",
-    #["Show Instruction","Landmark identification","Show the #source code", "About"]
     
-    # if choice == "Sign Language Recognition(Image)":
-    #     read_me_0.empty()
-    #     read_me.empty()
-    #     read_repo.empty()
-    #     sign_recognition_image()
-        
-    if choice == "Sign Language Recognition(Video)":
+    st.sidebar.title("Select Activity")
+    choice  = st.sidebar.selectbox("MODE",("About","Instructions","Sign Language Recognition", "Teach me a Sign")) # "Sign Language Recognition(Image)",
+    #["Show Instruction","Landmark identification","Show the #source code", "About"]
+    if choice == "About":
+        print()
+    if choice == "Sign Language Recognition":
+        img_0.empty()   
+        img_1.empty()  
+        line_0.empty() 
         read_me_0.empty()
         read_me.empty()
         read_repo.empty()
+        
         file_code =sign_recognition_video()
-    elif choice == "About":
-        print()
+        
+    elif choice == "Teach me a Sign":
+        img_0.empty()   
+        img_1.empty()  
+        line_0.empty() 
+        read_me_0.empty()
+        read_me.empty()
+        read_repo.empty()
+   
+        sign_recognition_Video_retraining()
         
 
 if __name__ == '__main__':
